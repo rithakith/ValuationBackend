b62f59c19bc33e6b142ef05f15a330b97ad11399
abeyweerasd.22 <abeyweerasd.22@uom.lk>
2025-05-11 23:47:20 +0530

    RA,RO,RB GET

--- Models/RatingRequest.cs
+++ Models/RatingRequest.cs
@@ -1,15 +1,22 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace ValuationBackend.Models
 {
     public class RatingRequest
     {
-        public int Id { get; set; }  // Auto-increment ID
-
-        public string RequestType { get; set; } // mass rating, rating assessment, etc.
+        public int Id { get; set; }
+        public string RequestType { get; set; } // Mass Rating, etc.
         public string RatingReferenceNo { get; set; }
         public string LocalAuthority { get; set; }
         public int YearOfRevision { get; set; }
         public string Status { get; set; }
+
+
+
+
+
+
     }
 }
